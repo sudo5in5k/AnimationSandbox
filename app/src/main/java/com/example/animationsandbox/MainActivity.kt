39728fc650fc8158f0da5a646f453795815cd8e4
ussy <sudo5in5k@gmail.com>
2019-11-30 20:58:13 +0900

    init

---
+++ app/src/main/java/com/example/animationsandbox/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.animationsandbox
+
+import android.graphics.drawable.TransitionDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        transition_iv.setOnTouchListener { _, _ ->
+            val td = transition_iv.background as TransitionDrawable?
+            td?.startTransition(1000)
+            td?.reverseTransition(1000)
+            false
+        }
+
+        fadein_iv.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fadein))
+    }
+}
